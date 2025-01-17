13e541006b3e1b18c6c4959af203d2e845add57e
MuhammadHamza9025 <mhamza9025@gmail.com>
2025-01-17 15:22:19 +0500

    jj

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const App = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email }),
       });
 
       const data = await response.json();
